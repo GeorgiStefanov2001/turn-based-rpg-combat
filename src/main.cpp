1eb4327a935f59dfcc72d00cb2562c4339221bca
George Stefanov <georgi.stefanov2001@gmail.com>
2022-12-11 19:16:22 +0200

    chore: Remove commented out code in main

--- src/main.cpp
+++ src/main.cpp
@@ -42,18 +42,6 @@ int main()
             logged_in_menu.display(logged_in, user, db_manager, db);
         }
 
-        // Knight attacker("Gosho", "Male", 50);
-        // Knight attacked("Pesho", "Male", 231);
-
-        // std::cout << attacker << std::endl;
-
-        // std::map<std::string, int> attack_req;
-        // attack_req.insert(std::pair<std::string, int>("Strength", 10));
-        // attacker.attack(attacked, "slash", 15, 10, attack_req);
-        // std::cout << attacked.get_current_vigor() << std::endl;
-
-        // std::cout << attacker.get_available_attacks().at("short_slash").at("faith_req") << std::endl;
-
         db_manager.close_database_connection(db);
     }
     catch (DatabaseException e)
